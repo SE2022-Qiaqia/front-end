6565c9fb54f14f7672b3d41b9e56a5cb23e7ec91
SalHe Li <SalHe@qq.com>
2022-05-25 14:56:53 +0800

    rename CourseScheduleReal -> CourseSchedule

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
 import { NGrid, NGridItem } from 'naive-ui';
 import UserBrief from '../components/UserBrief.vue';
-// import CourseScheduleMock from '../components/CourseScheduleMock.vue';
-import CourseScheduleReal from '../components/CourseScheduleReal.vue';
+import CourseSchedule from '../components/CourseSchedule.vue';
 import { injectStore } from '../store';
 
 const store = injectStore();
@@ -14,8 +13,7 @@ const schedules = store.state.schedules;
 <template>
   <n-grid :cols="12" :x-gap="12">
     <n-grid-item :offset="1" :span="8">
-      <!-- <course-schedule-mock /> -->
-      <course-schedule-real :schedules="schedules" />
+      <course-schedule :schedules="schedules" />
     </n-grid-item>
     <n-grid-item :span="2">
       <user-brief :user="student" />
